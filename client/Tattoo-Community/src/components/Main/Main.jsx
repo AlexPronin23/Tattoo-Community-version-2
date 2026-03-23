0c6c6aa5b69bcc4360f8cc05f94dadb782892405
AlexPronin23 <apronin2005@gmail.com>
2026-03-23 22:06:38 +0700

    Main component : Services block is completed

--- client/Tattoo-Community/src/components/Main/Main.jsx
+++ client/Tattoo-Community/src/components/Main/Main.jsx
@@ -1,9 +1,11 @@
 import Community from "./Community/Community";
+import Services from "./Services/Services";
 const Main = () => {
     return (
 
         <main className="main">
             <Community/>
+            <Services/>
         </main>
 
      );
---
+++ client/Tattoo-Community/src/components/Main/Services/Services.jsx
@@ -0,0 +1,58 @@
+import './style.scss'
+
+// Images
+
+import FirstPicture from '../../../assets/img/Main/Services/FirstPicture.png'
+import SecondPicture from '../../../assets/img/Main/Services/SecondPicture.png'
+import ThirdPicture from '../../../assets/img/Main/Services/ThirdPicture.png'
+
+const Services = () => {
+    return ( 
+        <section className="services">
+
+            <div className="container">
+
+                <div className="services_wrapper">
+
+                    <h1 className="title services_title">Услуги</h1>
+
+                 <div className="services_card">
+
+                    <div className="card">
+
+                        <img src={FirstPicture} alt="First Picture" className="card_img" />
+                        <p className="card_text">
+                            Профиль мастера
+                        </p>
+
+                    </div>
+
+                    <div className="card">
+
+                         <img src={SecondPicture} alt="Second Picture" className="card_img" />
+                          <p className="card_text">
+                            Продвижение бренда
+                          </p>
+
+                    </div>
+
+                    <div className="card">
+
+                         <img src={ThirdPicture} alt="Third Picture" className="card_img" />
+                          <p className="card_text">
+                            Консультация у чат бота
+                          </p>
+                          
+                    </div>
+
+                </div>
+
+                </div>
+
+            </div>
+
+        </section>
+     );
+}
+ 
+export default Services;
