aaa1faae9585fc11d68b6ba141ad4183d181c6cd
AlexPronin23 <apronin2005@gmail.com>
2026-03-27 23:29:40 +0700

    Main Component: Footer block is completed

--- client/Tattoo-Community/src/components/Footer/Footer.jsx
+++ client/Tattoo-Community/src/components/Footer/Footer.jsx
@@ -1,6 +1,72 @@
+import './style.scss'
+
+// Picture
+import FooterLogo from '../../assets/icon/Footer/FooterLogo.svg'
+import VK from '../../assets/icon/Footer/VK.svg'
+
 const Footer = () => {
     return ( 
-        <h1>Footer</h1>
+        
+        <footer className="footer">
+
+            <div className="container">
+
+                <div className="footer_wrapper">
+
+                    {/* Здесь потом будет отдельный компонент Logo */}
+                    <a href="" className="footer_logo">
+                        <img src={FooterLogo} alt="Footer Logo" />
+                    </a>
+
+
+                    {/* Здесь потом будет отдельный компонент Nav */}
+                    <nav className="footer_nav">
+
+                        <ul className="footer_items">
+
+                            <li className="footer_item">
+                                <a href="#" className="footer_link">
+                                    О нас
+                                </a>
+                            </li>
+                            <li className="footer_item">
+                                <a href="#" className="footer_link">
+                                    Контакты
+                                </a>
+                            </li>
+                            <li className="footer_item">
+                                <a href="#" className="footer_link">
+                                    Тату мастера
+                                </a>
+                            </li>
+                            <li className="footer_item">
+                                <a href="#" className="footer_link">
+                                    Услуги
+                                </a>
+                            </li>
+
+                        </ul>
+
+                    </nav>
+
+                    <div className="footer_social">
+
+                        <p className="footer_phone"> +79521529988 </p>
+
+                        <a href="#" className="footer_vk">
+                            <img src={VK} alt="VK" />
+                        </a>
+
+                        
+
+                    </div>
+                
+
+                </div>
+
+            </div>
+
+        </footer>
      );
 }
  
